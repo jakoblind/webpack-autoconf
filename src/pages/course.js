e658e4fd6775199d5a73a4dc396477a84668a77c
Jakob Lind <karl.jakob.lind@gmail.com>
2018-12-18 09:02:45 +0100

    new texts

--- src/pages/course.js
+++ src/pages/course.js
@@ -10,30 +10,32 @@ export default () => {
       <div className={styles.webpackConfigContainer}>
         <h1 className={styles.large}>Time to learn webpack properly</h1>
         <p>
-          You have clicked around in this tool for a while. But you still can't
-          wrap your head around how webpack works.
+          You want to build modern, awesome web apps. And you know you must
+          learn webpack to be able to do it.
         </p>
-        <ul>
-          <li>"What is the .babelrc file?"</li>
-          <li>
-            "What is a loader and what is a plugin? what's the difference?"
-          </li>
-          <li>"They want me to put CSS and images in the bundle?!"</li>
-        </ul>
-
         <p>
-          To learn webpack properly you must get your hands dirty and do the
-          work. It's not enough to glance over some generated webpack configs.
+          But webpack is really advanced and difficult to learn. The config file
+          is huge. It's really hard to know why you need all that config.
+        </p>
+        <p>
+          Why do you need both a '.babelrc' file and a webpack.config.js file?
+          And why should you put CSS and images in the JavaScript bundle?
         </p>
 
         <h3>Free webpack email course</h3>
-        <p>Yes, it's a build tool. Yes, you need a course to learn it.</p>
-
         <p>
-          With this email course you will learn webpack from the ground up.
-          Using industry best practices. <i>And it's free!</i>
+          With this email course we start from scratch and you'll learn webpack
+          from the ground up.
         </p>
 
+        <p>
+          You start with a clean slate and build webpack configs with your bare
+          hands. Coding yourself is the best way to learn webpack.
+        </p>
+        <p>
+          You will be using industry best practices and latest version of
+          webpack (version 4).
+        </p>
         <p>You get 5 emails in 5 days.</p>
         <ul>
           <li>Lesson 1: What does webpack do? (an overview)</li>
