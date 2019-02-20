779ca5872c846d2c543e8e8e897d8dc2d06e4f47
Jakob Lind <karl.jakob.lind@gmail.com>
2019-02-20 12:19:27 +0100

    add scss and less

--- src/templates/styling.js
+++ src/templates/styling.js
@@ -2,3 +2,19 @@ export const css = `body {
   color: white;
   background-color: black;
 }`
+
+export const scss = `$primary-color: white;
+$bg: white;
+
+body {
+  color: $primary-color;
+  background-color: $bg;
+}`
+
+export const less = `@primary-color: white;
+@bg: white;
+
+body {
+  color: @primary-color;
+  background-color: @bg;
+}`
