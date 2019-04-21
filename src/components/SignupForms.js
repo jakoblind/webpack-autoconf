8bb3233ccbde079d12bcad74b375275204a27d65
Jakob Lind <karl.jakob.lind@gmail.com>
2019-04-21 08:27:44 +0200

    added convertkit for email course and genericform

--- src/components/SignupForms.js
+++ src/components/SignupForms.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles.module.css'
 
-export const SignupForm = ({ buttonText, buttonStyle, signupText, dripId }) => (
+const SignupForm = ({ buttonText, buttonStyle, signupText, dripId }) => (
   <div className={styles.signupFormArea}>
     <form
       action={`https://www.getdrip.com/forms/${dripId}/submissions`}
@@ -41,6 +41,83 @@ export const SignupForm = ({ buttonText, buttonStyle, signupText, dripId }) => (
   </div>
 )
 
+function ConvertKitSignupForm({
+  children,
+  buttonText,
+  buttonStyle,
+  signupText,
+  formId = '915821',
+}) {
+  const [errorMessage, setErrorMessage] = useState('')
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+
+  const clearError = () => {
+    if (email !== '' && name !== '') {
+      setErrorMessage('')
+    }
+  }
+
+  const onSubmit = e => {
+    if (email === '' || name === '') {
+      setErrorMessage('Please no empty inputs')
+      e.preventDefault()
+    }
+  }
+  return (
+    <div className={styles.signupFormArea}>
+      <form
+        onSubmit={onSubmit}
+        method="post"
+        action={`https://app.convertkit.com/forms/${formId}/subscriptions`}
+      >
+        {children}
+        <div className={styles.signupFormValidationError}>{errorMessage}</div>
+        <div>
+          <input
+            className={styles.signupField}
+            onChange={e => {
+              clearError()
+              setEmail(e.target.value)
+            }}
+            style={
+              errorMessage && email === '' ? { border: '1px solid red' } : {}
+            }
+            autofocus={true}
+            placeholder="Your Email"
+            name="email_address"
+            type="email"
+          />
+        </div>
+        <div>
+          <input
+            className={styles.signupField}
+            onChange={e => {
+              clearError()
+              setName(e.target.value)
+            }}
+            style={
+              errorMessage && name === '' ? { border: '1px solid red' } : {}
+            }
+            name="first_name"
+            placeholder="Your Name"
+          />
+        </div>
+        <input type="hidden" name="api_key" value="qEqWamI6-oY7MOPZSU7qfw" />
+        <div className={styles.signupButtonArea}>
+          <input
+            className={buttonStyle || styles.myButton}
+            type="submit"
+            value={buttonText || 'Send Me Lesson 1'}
+          />
+          <br />
+          {signupText}
+        </div>
+      </form>
+    </div>
+  )
+}
+
 export function withHidden(WrappedComponent, text) {
   return class extends React.Component {
     constructor(props) {
@@ -77,9 +154,9 @@ export const SampleChapterSignupForm = ({ buttonText }) => (
 )
 
 export const GenericSignupForm = ({ buttonText }) => (
-  <SignupForm buttonText={buttonText} dripId={'81916834'} />
+  <ConvertKitSignupForm buttonText={'Sign up'} />
 )
 
 export const CourseSignupForm = ({ buttonText }) => (
-  <SignupForm buttonText={buttonText} dripId={'138671466'} />
+  <ConvertKitSignupForm buttonText={buttonText} formId="917789" />
 )
