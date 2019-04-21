ec66a34d67ab4d8c3cafe9ba410735c70e71a303
Jakob Lind <karl.jakob.lind@gmail.com>
2019-04-21 08:26:34 +0200

    add thankyou for signing up page

---
+++ src/pages/thank-you-for-signing-up.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../components/layout'
+import styles from '../styles.module.css'
+
+function App(props) {
+  return (
+    <Layout title="Thank you for signing up!">
+      <div className={styles.webpackConfigContainer}>
+        <h1 className={styles.large}>Thank you for signing up!</h1>
+        <p>Good stuff will be delivered straight to your inbox.</p>
+      </div>
+    </Layout>
+  )
+}
+
+export default App
