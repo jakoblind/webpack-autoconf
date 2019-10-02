52800aab6c4158123fd1be371e075838e67f27e4
jacekk <github.com@jacek-k.net>
2019-10-02 22:06:04 +0200

    chore: enabled test using some styles and files;

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
+      '<rootDir>/src/__mocks__/fileMock.js',
   },
   testPathIgnorePatterns: ['node_modules', '.cache'],
   transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
