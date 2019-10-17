67b07ed9e1c07e568ded5c61c686e9d9a97a9e50
jacekk <github.com@jacek-k.net>
2019-10-17 10:31:51 +0200

    #45: improved formatting in one of changed files;

--- src/configurator/project-generator.test.js
+++ src/configurator/project-generator.test.js
@@ -4,24 +4,24 @@ import projectGenerator from './project-generator';
 test('React project', () => {
   const project = projectGenerator(['React']);
 
-  expect(project['.babelrc']).not.toBeNull()
-  expect(_.size(project)).toBe(7)
-})
+  expect(project['.babelrc']).not.toBeNull();
+  expect(_.size(project)).toBe(7);
+});
 
 test('Vue project', () => {
   const project = projectGenerator(['Vue']);
 
-  expect(project['src/App.vue']).toBeDefined()
-  expect(_.size(project)).toBe(7)
+  expect(project['src/App.vue']).toBeDefined();
+  expect(_.size(project)).toBe(7);
 
   const projectWithTypescript = projectGenerator(['Vue', 'Typescript']);
 
-  expect(projectWithTypescript['vue-shim.d.ts']).toBeDefined()
-  expect(_.size(projectWithTypescript)).toBe(9)
-})
+  expect(projectWithTypescript['vue-shim.d.ts']).toBeDefined();
+  expect(_.size(projectWithTypescript)).toBe(9);
+});
 
 test('Empty project', () => {
   const project = projectGenerator([]);
 
-  expect(_.size(project)).toBe(6)
-})
+  expect(_.size(project)).toBe(6);
+});
