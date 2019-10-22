8e51919db0c4ad2e18ac0a12c80cd1bcee6df45c
AJay Kumar Pathuri <ajr1396@gmail.com>
2019-10-22 14:36:56 +0530

    Updated readme template

--- src/templates/rollup/index.js
+++ src/templates/rollup/index.js
@@ -4,7 +4,30 @@ export const getReadMeFile = (name, features) => {
   return `
 ## ${name}
 
-Hello
+## Building and running on localhost
+
+First install dependencies:
+
+\`\`\`sh
+npm install
+\`\`\`
+
+To create a development in watch mode:
+
+\`\`\`sh
+npm run watch
+\`\`\`
+
+
+To create a production build:
+
+\`\`\`sh
+npm run build
+\`\`\`
+
+## Credits
+
+Made with [createapp.dev](https://createapp.dev/)
 
   `;
 };
