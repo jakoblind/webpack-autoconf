4fabf69afac6ad179e264e86ee7ec55b5d6922cd
jacekk <github.com@jacek-k.net>
2019-10-23 09:20:30 +0200

    #45: one more small refactor;

--- src/components/FileBrowser.js
+++ src/components/FileBrowser.js
@@ -177,8 +177,7 @@ class FileBrowser extends React.Component {
   render() {
     const { fileContentMap } = this.props;
     const fileContent = _.get(fileContentMap, this.state.selectedFile, '');
-    const extensionMatch = this.state.selectedFile.match(extensionRegex);
-    const extension = _.isArray(extensionMatch) ? extensionMatch[0] : null;
+    const extension = _.head(this.state.selectedFile.match(extensionRegex));
 
     return (
       <div className={styles.fileBrowser} id="file-browser">
