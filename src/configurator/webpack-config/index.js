9e3758b41658dfb623b4b8260d6efbf53ccde1f9
Jakob Lind <karl.jakob.lind@gmail.com>
2020-06-24 11:58:44 +0200

    added mocha

--- src/configurator/webpack-config/index.js
+++ src/configurator/webpack-config/index.js
@@ -215,6 +215,27 @@ export default (() => {
         }
       }
     },
+    Mocha: {
+      group: "Unit tests",
+      devDependencies: configItems => {
+        return ["mocha"];
+      },
+      files: configItems => ({
+        "test/test.js": `var assert = require('assert');
+  describe('Array', function () {
+    describe('#indexOf()', function () {
+      it('should return -1 when the value is not present', function () {
+        assert.equal([1, 2, 3].indexOf(4), -1);
+      });
+    });
+  });`
+}),
+      packageJson: {
+        scripts: {
+          test: "mocha"
+        }
+      }
+    },
     Babel: {
       group: "Transpiler",
       babel: (babelConfig, configItems) => ({
